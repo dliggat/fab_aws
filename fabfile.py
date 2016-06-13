3fb5c7bd7fe374ad347d8d5dd5e41aed437a618f
Dave Liggat <dave@liggat.org>
2016-06-12 21:01:16 -0700

    Adding comments

--- fabfile.py
+++ fabfile.py
@@ -16,14 +16,15 @@ from fabric.api import task
 from fabric.contrib.console import confirm
 from fabric.utils import abort
 
-# Configure PyYaml to create ordered dicts
+
+# Configure PyYaml to preserve order in dictionaries (ensures CloudFormation templates render
+# in an expected top-level key order).
 _mapping_tag = yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG
 
 def construct_ordered_mapping(loader, node, deep=False):
     if isinstance(node, yaml.MappingNode):
         loader.flatten_mapping(node)
     return odict(loader.construct_pairs(node, deep))
-
 def construct_yaml_ordered_map(loader, node, deep=False):
     data = odict()
     yield data
@@ -32,17 +33,36 @@ def construct_yaml_ordered_map(loader, node, deep=False):
 
 yaml.add_constructor(_mapping_tag, construct_yaml_ordered_map)
 
+
+# Various constants and config setup.
 ROOT_DIR     = os.path.dirname(__file__)
 INPUT_DIR    = os.path.join(ROOT_DIR, 'input')
-INPUT_FILES  = glob.glob(os.path.join(INPUT_DIR, '*.yaml.jinja'))
+INPUT_EXT    = '.yaml.jinja'
+INPUT_FILES  = glob.glob(os.path.join(INPUT_DIR, '*{0}'.format(INPUT_EXT)))
 
 OUTPUT_DIR   = os.path.join(ROOT_DIR, '_output')
 OUTPUT_EXT   = '.template'
 OUTPUT_FILES = glob.glob(os.path.join(OUTPUT_DIR, '*{0}'.format(OUTPUT_EXT)))
 
+CONFIG_DIR   = os.path.join(ROOT_DIR, 'config')
+CONFIG_FILES = glob.glob(os.path.join(CONFIG_DIR, '*{0}'.format('.yaml')))
 
+def load_config():
+    """Load the config from the config directory into a deep dict, keyed by stack type."""
+    config = {}
+    for config_file in CONFIG_FILES:
+        with open(config_file, 'r') as config_file_contents:
+            data = yaml.load(config_file_contents.read())
+            config[os.path.basename(config_file).split('.')[0]] = data
+    logger.info('Loaded config: %s', config)
+    return config
+
+
+# Fabric tasks.
 @task(default=True)
 def render():
+    """Render yaml.jinja to JSON, via the loaded config."""
+    config = load_config()
     if not INPUT_FILES:
         abort('No YAML files present in directory')
 
@@ -50,22 +70,27 @@ def render():
         logger.info('Created directory: %s', OUTPUT_DIR)
         os.makedirs(OUTPUT_DIR)
 
+    # Load the Jinja environment.
+    env = jinja2.Environment(trim_blocks=True, lstrip_blocks=True, undefined=jinja2.StrictUndefined)
+    env.loader = jinja2.FileSystemLoader(INPUT_DIR)
+
+    # For each file in the input directory; run through a Jinja render and convert to JSON.
     for input_file in INPUT_FILES:
-        with open(input_file, 'r') as jinja_contents:
+        basename = os.path.basename(input_file).split('.')[0]
+        template = env.get_template('{0}{1}'.format(basename, INPUT_EXT))
+        rendered = template.render(**config)
+        data = yaml.load(rendered)
 
-            jenv = jinja2.Environment(trim_blocks=True, lstrip_blocks=True, undefined=jinja2.StrictUndefined)
-            jenv.loader = jinja2.FileSystemLoader('./jinja')
+        output_str  = json.dumps(data, indent=2)
+        output_file = os.path.join(OUTPUT_DIR, os.path.basename(input_file).split('.')[0]) + OUTPUT_EXT
+        with open(output_file, 'w') as output_contents:
+            output_contents.write(output_str)
+            logger.info('Converted %s to %s', os.path.basename(input_file), os.path.basename(output_file))
 
-            data = yaml.load(jinja_contents.read())
-            output_str = json.dumps(data, indent=2)
-            print("yo", os.path.basename(input_file).split('.')[0])
-            output_file = os.path.join(OUTPUT_DIR, os.path.basename(input_file).split('.')[0]) + OUTPUT_EXT
-            with open(output_file, 'w') as output_contents:
-                output_contents.write(output_str)
-                logger.info('Converted %s to %s', os.path.basename(input_file), os.path.basename(output_file))
 
 @task
 def validate():
+    """Validates the rendered template against the CloudFormation API."""
     client = boto3.client('cloudformation')
     for output_file in OUTPUT_FILES:
         with open(output_file, 'r') as output_contents:
@@ -78,7 +103,12 @@ def validate():
 
 @task
 def launch(template_name=None, stack_name=None):
+    """Creates or updates a CloudFormation stack based on the supplied template type name.
 
+    Args:
+        template_name (str): The name of the template from the input directory.
+        stack_name (str): The stack name to use for CloudFormation.
+    """
     if not template_name:
         abort('Must provide template')
     if not stack_name:
@@ -111,8 +141,6 @@ def launch(template_name=None, stack_name=None):
 
 @task
 def clean():
+    """"Deletes all the rendered output files."""
     for f in OUTPUT_FILES:
         os.remove(f)
-
-
-
