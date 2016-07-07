c637c0eba10d5444911b7f940025d996bde22dad
Dave Liggat <dave@liggat.org>
2016-07-07 13:50:33 -0700

    Added state_tracker to interact with dynamo table

---
+++ lambda/downtime_notifier/downtime_notifier/state_tracker.py
@@ -0,0 +1,64 @@
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+
+
+class StateTracker(object):
+
+    def __init__(self, checker, dynamo_table_name, timestamp):
+        """
+        Args:
+            checker: (Checker) a Checker object.
+            dynamo_table_name: (str) Name of the DynamoDB table to interrogate.
+            timestamp: (datetime) Time to associate with the new record to be written.
+        """
+        assert(all([checker, dynamo_table_name, timestamp]))
+        self.checker = checker
+        self.dynamo = boto3.resource('dynamodb')
+        self.timestamp = str(timestamp)
+        self.table = self.dynamo.Table(dynamo_table_name)
+        self._first_check = False
+        self._notify = False
+
+    def put_result(self):
+        """Records the latest value of the check to the result table."""
+        self._examine_latest()
+
+        # If it's either the first time around, or the state has changed, we should notify.
+        if self._first_check or self._previous_exceptional != self.checker.exceptional:
+            self._notify = True
+
+        # Now write this new value to the table.
+        item = {
+          'TargetId': self.checker.name,
+          'TargetUrl': self.checker.url,
+          'Timestamp': self.timestamp,
+          'IsExceptional': self.checker.exceptional
+        }
+        if self.checker.exceptional:
+            item['message'] = self.checker.message
+        self.table.put_item(Item=item)
+
+
+    def _examine_latest(self):
+        """Examines the previous value of the check for this Checker."""
+        response = self.table.query(
+            Limit=1,
+            ScanIndexForward=False,
+            ConsistentRead=True,
+            KeyConditionExpression=Key('TargetId').eq(self.checker.name))
+
+
+        if response['Count']:  # There is a pre-existing check value.
+            self._previous_exceptional = response['Items'][0]['IsExceptional']
+            self._previous_message = response['Items'][0].get('message')
+        else:                  # This is the first time we've seen this value.
+            self._first_check = True
+
+        print(response)
+
+
+    @property
+    def notify(self):
+        """(bool) Whether or not the tracked situation warrants notification."""
+        return self._notify
+
--- lambda/downtime_notifier/index.py
+++ lambda/downtime_notifier/index.py
@@ -1,9 +1,11 @@
 import boto3
+from boto3.dynamodb.conditions import Key, Attr
 import datetime
 import pprint
 
 from downtime_notifier import configuration
 from downtime_notifier import Checker
+from downtime_notifier import StateTracker
 
 
 MAX_LEN = 100
@@ -12,28 +14,42 @@ CONFIG = configuration()
 
 def handler(event, context):
     """Entry point for the Lambda function."""
+
     print('Using configuration: {0}'.format(CONFIG))
-    checkers = []
+    trackers = []
+    timestamp = datetime.datetime.now()
+
+    # Build a Checker object, and associate with a StateTracker.
     for site in CONFIG.get('env', {}).get('sites', []):
         c = Checker(**site)
         c.check()
-        checkers.append(c)
+        t = StateTracker(c, CONFIG['env']['dynamo_table'], timestamp)
+        trackers.append(t)
 
-    checkers = [r for r in checkers if r.exceptional]
+    # Record the outcome in the result table.
+    for tracker in trackers:
+        tracker.put_result()
+
+    # Notify the SNS topic if any tracker indicates thusly.
+    checkers = [t.checker for t in trackers if t.notify]
     if checkers:
-        notify(checkers)
+        if any([c.exceptional for c in checkers]):
+            title_prefix = CONFIG['env']['downtime_detected_prefix']
+        else:
+            title_prefix = CONFIG['env']['state_changed_prefix']
+        notify(checkers, title_prefix)
     else:
         print("All checks passed: {0}.".format(datetime.datetime.now()))
 
 
-def notify(checkers):
+def notify(checkers, title_prefix):
     """Craft a message about the site downtime, and publish to the SNS topic.
 
     Args:
         checkers: (list) Sites which failed the check.
+        title_prefix: (str) A prefix for the SNS message.
     """
-    subject = "{0} {1}".format(CONFIG['env']['subject_prefix'],
-                               ', '.join([r.name for r in checkers]))
+    subject = "{0} {1}".format(title_prefix, ', '.join([r.name for r in checkers]))
     message = '\n\n'.join(
         ['{0}) {1} ({2}): {3}'.format(i, r.name, r.url, r.message) for i, r in enumerate(checkers)])
 
