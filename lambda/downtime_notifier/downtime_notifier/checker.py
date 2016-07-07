ece11ab14b3ce38e7c23e178b2138d86c2810662
Dave Liggat <dave@liggat.org>
2016-07-07 14:41:33 -0700

    Adding exponential backoff

--- lambda/downtime_notifier/downtime_notifier/checker.py
+++ lambda/downtime_notifier/downtime_notifier/checker.py
@@ -1,9 +1,15 @@
 import requests
+import retrying
 
 
 class Checker(object):
 
     TIMEOUT = 10
+    class UnexpectedHttpStatusError(Exception):
+        pass
+    class ExpectedTextNotFoundError(Exception):
+        pass
+
 
     def __init__(self, url=None, name=None, expected_code=200, expected_text=None):
         """
@@ -31,11 +37,10 @@ class Checker(object):
         """(bool) True if the GET behaved as expected/desired, else False."""
         return self._exceptional
 
-
     def check(self):
         """Run a GET on the url, and build a message detailing any exceptional circumstances."""
         try:
-            req = requests.get(self.url, timeout=self.TIMEOUT, allow_redirects=False)
+            self._attempt_request()
 
         except requests.exceptions.ConnectionError as e:
             self._exceptional = True
@@ -52,21 +57,33 @@ class Checker(object):
             self._message = 'Got an unspecified error connecting to {0}; result: {1}'.format(self.name, e.message)
             return
 
-        # Check the status code against what was expected.
-        if req.status_code != self.expected_code:
+        except (Checker.UnexpectedHttpStatusError, Checker.ExpectedTextNotFoundError) as e:
             self._exceptional = True
-            self._message = 'Expected HTTP {0} connecting to {1}; got {2} instead.'.format(
-                self.expected_code, self.name, req.status_code)
-            return
-
-        # Validate the text against the expectation, if one was supplied.
-        if self.expected_text and self.expected_text not in req.text:
-            self._exceptional = True
-            self._message = 'Expected to find "{0}" in request to {1}; was missing'.format(
-                self.expected_text, self.name)
+            self._message = e.message
             return
 
         # Looks like everything worked.
         self._message = 'Successfully connected to {0}; got response {1}!'.format(self.name, self.expected_code)
         return
+
+    @retrying.retry(
+        stop_max_attempt_number=5,
+        wait_exponential_multiplier=500,
+        wait_exponential_max=5000)
+    def _attempt_request(self):
+        """Attempt to connect; use exponential backoff if an error occurs."""
+        print('Attempting a request for {0}'.format(self.name))
+        req = requests.get(self.url, timeout=self.TIMEOUT, allow_redirects=False)
+
+        # Check the status code against what was expected.
+        if req.status_code != self.expected_code:
+            message = 'Expected HTTP {0} connecting to {1}; got {2} instead.'.format(
+                self.expected_code, self.name, req.status_code)
+            raise Checker.UnexpectedHttpStatusError(message)
+
+        # Validate the text against the expectation, if one was supplied.
+        if self.expected_text and self.expected_text not in req.text:
+            message = 'Expected to find "{0}" in request to {1}; was missing'.format(
+                self.expected_text, self.name)
+            raise Checker.ExpectedTextNotFoundError(message)
 
